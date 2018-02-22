9c90f8a513f0aa070ad4bc5b8e7a17662f95f02d
helgard88 <merlin-88@ukr.net>
2018-02-22 03:33:28 +0200

    Revert "fix hosts"
    This reverts commit 07ecfd1e6dc67cba8e4c031fe5e414aed16b64d4.
    
    Undo changes to sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -17,30 +17,12 @@ JAVA_HOME="/opt/jdk"
 JRE_HOME="/opt/jdk/jre"
 IPT_CONF="/etc/sysconfig/iptables-config"
 
-###################################################
-#  		HOSTS	  	      
-###################################################
-sudo -s
-echo "
-192.168.56.150	db.local
-192.168.56.170	jenkins.local
-192.168.56.180	sonar.local
-192.168.56.160	web.local">>/etc/hosts
-
-###################################################
-#		BASE SECTION		  	  		  #
-###################################################
-rm -fr /etc/localtime
-ln -s /usr/share/zoneinfo/Europe/Kiev /etc/localtime
-yum install -y ntpdate
-ntpdate -u pool.ntp.org
-# yum install -y wget
-
 #########################
 #JavaSetup section	#
 #################################################
 #scrip downloads and setup Oracle Java 1.8.0.162#
 #################################################
+sudo -s
 cd /opt/
 #downloading and unpaking
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"  "http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.tar.gz"
@@ -97,6 +79,7 @@ fs.file-max = 65536">>/etc/sysctl.conf
 echo "
 sonar	soft nofile   	65536
 sonar   soft   nproc    2048
+sonar soft nofile 65536
 sonar hard nofile 65536">>/etc/security/limits.conf
 ###############################
 
@@ -106,6 +89,7 @@ sonar hard nofile 65536">>/etc/security/limits.conf
 sudo ln -s $SONAR_HOME/bin/linux-x86-64/sonar.sh /usr/bin/sonar
 #setup sonar as service
 cat>/etc/systemd/system/sonar.service<<EOF
+echo"
 [Unit]
 Description=SonarQube service
 After=network.target network-online.target
