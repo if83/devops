eea21e3030c595e122263af3d9850427371f8a6c
oleksdiam <oleksdiam@yahoo.com>
2018-03-13 13:20:45 +0200

    changing hostname

--- pclient.sh
+++ pclient.sh
@@ -45,6 +45,9 @@ echo "
 $PMASTER_IP    $PMASTER_DNAME
 " >> /etc/hosts
 
+echo "$HOSTNAME" > /etc/hostname
+
+
 rpm -Uvh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
 
 yum install -y puppet-agent
